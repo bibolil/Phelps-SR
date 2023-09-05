e05f70e1811f459974e2a2fcfb31200f66d2f610
Bilel Taboubi <bileltaboubi20@gmail.com>
2023-09-05 14:00:54 +0100

    removed unnecessary components

--- front-end/SR-ui/src/app/img.model.ts
+++ front-end/SR-ui/src/app/img.model.ts
@@ -3,7 +3,8 @@ export class Img
     constructor(
         public id: string,
         public imagePath: string,
-        public creationtime: Date
+        public creationtime: Date,
+        public thumbnail: string
     )
     {}
 
